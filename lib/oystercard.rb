6e1a0071bc9982cfc96c47e7523057147b151a5b
paulwallis42 <paulwallis42@gmail.com>
2016-04-21 16:09:37 +0100

    basic touch in and out functions passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
+  alias_method :in_journey?, :in_journey
 
   DEFAULT_BALANCE = 0
   BALANCE_LIMIT = 90
 
   def initialize
     @balance = DEFAULT_BALANCE
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -16,6 +18,14 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'has a zero balance on creation' do
-    expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
-  end
+
+    it 'has a zero balance on creation' do
+      expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
+    end
+
+    it 'is not in_journey on creation' do
+      expect(subject).not_to be_in_journey
+    end
 
   describe '#top_up' do
 
@@ -33,4 +38,28 @@ describe Oystercard do
     end
 
   end
+
+  describe '#touch_in' do
+
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'after touching in, card shows that it is in use' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+  end
+
+  describe '#touch_out' do
+
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'after touching out, card shows it is not in use' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
+  end
+
 end
